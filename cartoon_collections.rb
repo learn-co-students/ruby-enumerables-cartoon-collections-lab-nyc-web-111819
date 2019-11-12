11fc53938239007c8cd680fbf442af08cc64875d
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 20:53:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  i = 0
-  array.include?(cheese_types) {|cheese| puts cheese_types[i]}
+ cheese_types = ["cheddar", "gouda", "camembert"]
+ cheeses = %w[gouda cheddar camembert]
+ array.find {|any_cheese| cheeses.include?(any_cheese)}
 end
